d602a67707d2af00699e3541c7893e9c9da7cdfc
dpfflqhd <dpfflqhd@naver.com>
2021-05-12 16:34:06 +0900

    쓸모없는 프로젝트 삭제(김승환)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,49 +12,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView navi;
-    HomeFragment homeFragment;
-    FoodFragment foodFragment;
-    CultureFragment cultureFragment;
-    ChatFragment chatFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        navi = findViewById(R.id.bnv);
-        homeFragment = new HomeFragment();
-        foodFragment = new FoodFragment();
-        cultureFragment = new CultureFragment();
-        chatFragment = new ChatFragment();
-
-        executeFragment(homeFragment);
 
 
-        navi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                //선택한 메뉴아이템을 클릭시 FrameLayout에 출력
-                if(item.getItemId() == R.id.item_home){
-                    executeFragment(homeFragment);
-                } else if(item.getItemId() == R.id.item_food){
-                    executeFragment(foodFragment);
-                } else if(item.getItemId() == R.id.item_culture){
-                    executeFragment(cultureFragment);
-                } else if(item.getItemId() == R.id.item_talk){
-                    executeFragment(chatFragment);
-                }
-
-                //true:클릭한 메뉴아이템 포커싱 | false : 변화없음
-                return true;
-            }
-        });
-
-    }
-
-    private void executeFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
     }
 
 }
