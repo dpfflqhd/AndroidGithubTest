3cd683389a84c701e52c1987ad48fb852dc5a762
JaeTwoNew <jae297@naver.com>
2021-05-14 09:22:30 +0900

    시작페이지->로그인 다이얼로그 띄우기

---
+++ app/src/main/java/com/example/myapplication/LoginDialog.java
@@ -0,0 +1,66 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LoginDialog extends Dialog implements View.OnClickListener{
+
+    private Context mContext;
+    private TextView btn_cancel;
+    private TextView btn_ok;
+    private EditText edt_loginid, edt_loginpw;
+    private String loginid, loginpw;
+
+
+
+    public LoginDialog(@NonNull Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_dialog);
+
+        edt_loginid = findViewById(R.id.edt_loginid);
+        edt_loginpw = findViewById(R.id.edt_login_pw);
+
+        btn_cancel = (TextView) findViewById(R.id.btn_cancel);
+        btn_ok = (TextView) findViewById(R.id.btn_ok);
+
+        btn_cancel.setOnClickListener(this);
+        btn_ok.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            // 취소버튼
+            case R.id.btn_cancel:
+                dismiss(); // 다이얼로그 닫기
+                break;
+
+            // 로그인버튼
+            case R.id.btn_ok:
+                ((MainActivity) mContext).finish(); //메인 엑티비티 닫기
+                dismiss();
+                break;
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/myapplication/StartActivity.java
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -2,22 +2,49 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class StartActivity extends AppCompatActivity {
-    private Button btn_start_login, btn_start_join;
+public class StartActivity extends AppCompatActivity{
+
+    String dialog_id, dialog_pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        btn_start_login = findViewById(R.id.btn_start_login);
-        btn_start_join = findViewById(R.id.btn_start_join);
+        Button btn_start_login = (Button)findViewById(R.id.btn_start_login);
+        Button btn_start_join = (Button)findViewById(R.id.btn_start_join);
 
+
+
+        btn_start_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 다이얼로그 생성하고 보여주기
+                LoginDialog loginDialog = new LoginDialog(StartActivity.this);
+                loginDialog.show();
+
+                // 다이얼로그에서 로그인버튼 클릭 시 포토액티비티로 이동
+                loginDialog.findViewById(R.id.btn_ok).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            Intent photo_intent = new Intent(getApplicationContext(), PhotoActivity.class);
+                            startActivity(photo_intent);
+                    }
+                });
+            }
+        });
+
+        // 회원가입버튼 클릭 시
         btn_start_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,13 +52,6 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(join_intent);
             }
         });
-
-        btn_start_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent login_intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(login_intent);
-            }
-        });
     }
+
 }
